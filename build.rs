501d9b4048cf3bbfdaac42cb7df04bbc01602148
Alexis Lozano <alexis.pascal.lozano@gmail.com>
2019-08-14 08:09:30 +0200

    Removed sys dep sass

--- build.rs
+++ build.rs
@@ -1,26 +1,28 @@
 use std::env;
-use std::process::Command;
+use rsass::{compile_scss_file, OutputStyle};
+use std::fs;
 
 fn main() {
     let out_dir = env::var("OUT_DIR").unwrap();
 
     // Theming commons
-    Command::new("sass")
-        .args(&[
-            "--no-cache",
-            "src/www/app.scss",
-            &format!("{}/app.css", out_dir),
-        ])
-        .status()
-        .unwrap();
+    compile_write(
+        "src/www/app.scss",
+        &format!("{}/app.css", out_dir)
+    );
 
     // Breeze theme
-    Command::new("sass")
-        .args(&[
-            "--no-cache",
-            "src/www/breeze.scss",
-            &format!("{}/breeze.css", out_dir),
-        ])
-        .status()
-        .unwrap();
+    compile_write(
+        "src/www/breeze.scss",
+        &format!("{}/breeze.css", out_dir)
+    );
+}
+
+fn compile_write(path: &str, out: &str) {
+    let css = compile_scss_file(
+        path.as_ref(),
+        OutputStyle::Compressed
+    ).unwrap();
+
+    fs::write(out, css).unwrap();
 }
