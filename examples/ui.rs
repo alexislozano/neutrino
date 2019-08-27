27a6de047fa9c30bdd461a0c349dc88bc38aa705
Alexis Lozano <alexis.pascal.lozano@gmail.com>
2019-08-27 10:22:15 +0200

    Centered text in Button

--- examples/ui.rs
+++ examples/ui.rs
@@ -43,35 +43,32 @@ fn main() {
 
     ui2.add(Box::new(l2));
     ui2.add(Box::new(r2));
-    
+
     // UI 3
-    let mut ui3 = Container::new().direction(Direction::Horizontal).position(Position::Between).stretch();
+    let mut ui3 = Container::new().direction(Direction::Vertical).position(Position::Between).stretch();
 
-    let tl3 = Button::new("tl").text("TL").stretch();
-    let bl3 = Button::new("br").text("BL").stretch();
-    let tr3 = Button::new("tr").text("TR");
-    let br3 = Button::new("br").text("BR");
+    let b3 = Button::new("button3").text("Button").stretch();
+    ui3.add(Box::new(b3));
+    
+    // UI 4
+    let mut ui4 = Container::new().direction(Direction::Vertical).position(Position::End).stretch();
+    let mut t4 = Container::new().direction(Direction::Horizontal);
 
-    let mut l3 = Container::new().direction(Direction::Vertical);
-    let mut r3 = Container::new().direction(Direction::Vertical).position(Position::Between);
+    let b4 = Button::new("button4").text("Button").stretch();
+    t4.add(Box::new(b4));
 
-    l3.add(Box::new(tl3));
-    l3.add(Box::new(bl3));
-    r3.add(Box::new(tr3));
-    r3.add(Box::new(br3));
-
-    ui3.add(Box::new(l3));
-    ui3.add(Box::new(r3));
+    ui4.add(Box::new(t4));
 
     // Tabs
     let mut tabs = Tabs::new("root").stretch();
     tabs.add(("UI 1", Box::new(ui1)));
     tabs.add(("UI 2", Box::new(ui2)));
+    tabs.add(("UI 3", Box::new(ui3)));
 
     // Root
     let mut root = Container::new().direction(Direction::Horizontal);
     root.add(Box::new(tabs));
-    root.add(Box::new(ui3));
+    root.add(Box::new(ui4));
 
     let window = Window::new()
         .title("Custom")
