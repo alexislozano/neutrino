e0131e6d8249282b080e7fe70d25e03df3d6e260
fabrixxm <fabrix.xm@gmail.com>
2019-09-06 21:04:09 +0200

    Add interactivity to examples' Range and Progressbar

--- examples/demo_mod/listeners.rs
+++ examples/demo_mod/listeners.rs
@@ -4,9 +4,14 @@ use std::rc::Rc;
 use neutrino::utils::event::Key;
 use neutrino::widgets::menubar::{MenuBarListener, MenuBarState};
 use neutrino::widgets::tabs::{TabsListener, TabsState};
+use neutrino::widgets::range::{RangeListener, RangeState};
+use neutrino::widgets::progressbar::{ProgressBarListener, ProgressBarState};
+use neutrino::widgets::label::{LabelListener, LabelState};
+use neutrino::widgets::textinput::{TextInputListener, TextInputState};
 use neutrino::WindowListener;
 
-use super::models::Panes;
+use super::models::{Panes, RangeValue};
+
 
 pub struct MyWindowListener {
     panes: Rc<RefCell<Panes>>,
@@ -79,5 +84,78 @@ impl MenuBarListener for MyMenuBarListener {
                 }
             }
         }
+    }
+}
+
+
+pub struct MyRangeListener {
+    range: Rc<RefCell<RangeValue>>,
+}
+
+impl MyRangeListener {
+    pub fn new(range: Rc<RefCell<RangeValue>>) -> Self {
+        Self { range }
+    }
+}
+
+impl RangeListener for MyRangeListener {
+    fn on_update(&self, state: &mut RangeState) {
+        state.set_value(self.range.borrow().value());
+    }
+    fn on_change(&self, state: &RangeState) {
+        self.range.borrow_mut().set_value(state.value());
+    }
+}
+
+pub struct MyProgressBarListener {
+    range: Rc<RefCell<RangeValue>>,
+}
+
+impl MyProgressBarListener {
+    pub fn new(range: Rc<RefCell<RangeValue>>) -> Self {
+        Self { range }
+    }
+}
+
+impl ProgressBarListener for MyProgressBarListener {
+    fn on_update(&self, state: &mut ProgressBarState) {
+        state.set_value(self.range.borrow().value());
+    }
+}
+
+pub struct MyLabelListener {
+    range: Rc<RefCell<RangeValue>>,
+}
+
+impl MyLabelListener {
+    pub fn new(range: Rc<RefCell<RangeValue>>) -> Self {
+        Self { range }
+    }
+}
+
+impl LabelListener for MyLabelListener {
+    fn on_update(&self, state: &mut LabelState) {
+        let text = format!("{}%", self.range.borrow().value());
+        state.set_text(&text);
+    }
+}
+
+
+pub struct MyTextInputListener {
+    range: Rc<RefCell<RangeValue>>,
+}
+
+impl MyTextInputListener {
+    pub fn new(range: Rc<RefCell<RangeValue>>) -> Self {
+        Self { range }
+    }
+}
+
+impl TextInputListener for MyTextInputListener {
+    fn on_update(&self, state: &mut TextInputState) {
+        state.set_value(&self.range.borrow().value().to_string());
+    }
+    fn on_change(&self, state: &TextInputState) {
+        self.range.borrow_mut().set_value(state.value().parse().unwrap());
     }
 }
--- examples/demo_mod/models.rs
+++ examples/demo_mod/models.rs
@@ -15,3 +15,21 @@ impl Panes {
         self.value = value;
     }
 }
+
+pub struct RangeValue {
+    value: i32,
+}
+
+impl RangeValue {
+    pub fn new() -> Self {
+        Self { value: 0 }
+    }
+
+    pub fn value(&self) -> i32 {
+        self.value
+    }
+
+    pub fn set_value(&mut self, value: i32) {
+        self.value = value;
+    }
+}
