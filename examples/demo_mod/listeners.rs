5a532e274a6b62c8c62accb9cb0ac82256951e26
alexislozano <50751082+alexislozano@users.noreply.github.com>
2019-09-11 21:03:47 +0200

    Demo: interactive Range, ProgressBar and TextInput widgets (#30)
    * Fix container and menubar on osx 10.12
    
    * Add css rule to disable text selection
    
    * Add interactivity to examples' Range and Progressbar
    
    * add some comments
    
    * update demo_fluent to interactive Range
    
    * demo: prevent crash when user insert a non numeric value in TextInput
    
    * ProgressBarState value is always bw min and max
    
    * Added onchange in Range HTML for Windows to work

--- examples/demo_mod/listeners.rs
+++ examples/demo_mod/listeners.rs
@@ -4,10 +4,17 @@ use std::rc::Rc;
 use neutrino::utils::event::Key;
 use neutrino::widgets::menubar::{MenuBarListener, MenuBarState};
 use neutrino::widgets::tabs::{TabsListener, TabsState};
+use neutrino::widgets::range::{RangeListener, RangeState};
+use neutrino::widgets::progressbar::{ProgressBarListener, ProgressBarState};
+use neutrino::widgets::label::{LabelListener, LabelState};
+use neutrino::widgets::textinput::{TextInputListener, TextInputState};
 use neutrino::WindowListener;
 
-use super::models::Panes;
+use super::models::{Panes, RangeValue};
 
+/*
+ Window listener: waits for menu shortcuts
+*/
 pub struct MyWindowListener {
     panes: Rc<RefCell<Panes>>,
 }
@@ -30,6 +37,10 @@ impl WindowListener for MyWindowListener {
     }
 }
 
+/*
+ Tabs Listener: change current tab when user clicks on a tab label,
+ on a menu item or uses a shortcut
+*/
 pub struct MyTabsListener {
     panes: Rc<RefCell<Panes>>,
 }
@@ -50,6 +61,7 @@ impl TabsListener for MyTabsListener {
     }
 }
 
+/* Menu Bar Listener: waits for the user to select a menu item */
 pub struct MyMenuBarListener {
     panes: Rc<RefCell<Panes>>,
 }
@@ -79,5 +91,87 @@ impl MenuBarListener for MyMenuBarListener {
                 }
             }
         }
+    }
+}
+
+
+/* Range Listener: update RangeValue when the user scroll the Range widget */
+pub struct MyRangeListener {
+    range: Rc<RefCell<RangeValue>>,
+}
+
+impl MyRangeListener {
+    pub fn new(range: Rc<RefCell<RangeValue>>) -> Self {
+        Self { range }
+    }
+}
+
+impl RangeListener for MyRangeListener {
+    fn on_update(&self, state: &mut RangeState) {
+        state.set_value(self.range.borrow().value());
+    }
+    fn on_change(&self, state: &RangeState) {
+        self.range.borrow_mut().set_value(state.value());
+    }
+}
+
+/* Progress Bar Listener: update the Progress Bar value to the current
+RangeValue value*/
+pub struct MyProgressBarListener {
+    range: Rc<RefCell<RangeValue>>,
+}
+
+impl MyProgressBarListener {
+    pub fn new(range: Rc<RefCell<RangeValue>>) -> Self {
+        Self { range }
+    }
+}
+
+impl ProgressBarListener for MyProgressBarListener {
+    fn on_update(&self, state: &mut ProgressBarState) {
+        state.set_value(self.range.borrow().value());
+    }
+}
+
+
+/* Label Listenr: update the Label text to show the current RangeValue value,
+formatted as a percent */
+pub struct MyLabelListener {
+    range: Rc<RefCell<RangeValue>>,
+}
+
+impl MyLabelListener {
+    pub fn new(range: Rc<RefCell<RangeValue>>) -> Self {
+        Self { range }
+    }
+}
+
+impl LabelListener for MyLabelListener {
+    fn on_update(&self, state: &mut LabelState) {
+        let text = format!("{}%", self.range.borrow().value());
+        state.set_text(&text);
+    }
+}
+
+
+/* Text Input Listener: update the TextInput value to the
+current RangeValue value or set the RangeValue when the user
+changes the TextInput value */
+pub struct MyTextInputListener {
+    range: Rc<RefCell<RangeValue>>,
+}
+
+impl MyTextInputListener {
+    pub fn new(range: Rc<RefCell<RangeValue>>) -> Self {
+        Self { range }
+    }
+}
+
+impl TextInputListener for MyTextInputListener {
+    fn on_update(&self, state: &mut TextInputState) {
+        state.set_value(&self.range.borrow().value().to_string());
+    }
+    fn on_change(&self, state: &TextInputState) {
+        self.range.borrow_mut().set_value(state.value().parse().unwrap_or(0));
     }
 }
